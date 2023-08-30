5548568f85af65c2e2127b17a6a45a5deabbd419
Fer <fernanda23demarzo@gmail.com>
2023-08-30 01:17:26 -0300

    Api

--- src/data/api.jsx
+++ src/data/api.jsx
@@ -1,88 +1,254 @@
 export const data = {
     results: [
-        {
-          "Name": "Adivin Banderas",
-          "Logo": "https://v5.airtableusercontent.com/v1/19/19/1690927200000/lRkv-QfOn62pd7my2h8WtQ/lU0EU7x81-XchUeo7eI8TJy8G7LzlLEYWaHlZPpQym4tB8BPzb52iCU1CX5sbZRrctgkeJ7Hoepn16WGpt3nuDG_8fX-mZoRMj6f6iEsSko/IbhkYP0IP68rPjd9I2jHfah8e9YK8vIsc7WryKD9Zx4",
-          "Telefono": "952 31 60 22",
-          "Email": "jam@adivin.com",
-          "Region": "Andalucía",
-          "Provincia": "Córdoba",
-          "Produccion": [
-            "Publicidad exterior vallas y autobuses"
-          ],
-          "Tecnologia": [
-            "Serigrafía"
-          ],
-          "Especialidad": [
-            "Impresión"
-          ]
-        },
-        {
-          "Name": "3D Lenticular Factory",
-          "Logo": "https://v5.airtableusercontent.com/v1/19/19/1690927200000/XmLlLYUMOeehgTQogHrbsw/ID0eqqgjVGlvPfpFYX9DGfGOHbktneBfNb7kWsve7CPb6VpDPjM9VkclyMEEQYXfwYJuP4pTJGJVtOC_0Yfndg/h-E7EMRbcbQOX4AGA2RKRTCcngY9wegIaaXe98sZChY",
-          "Telefono": "917932125",
-          "Email": "info@3dlenticularfactory.com",
-          "Region": "Madrid",
-          "Produccion": [
-            "Impresión sobre soportes rígidos"
-          ],
-          "Tecnologia": [
-            "Diseño"
-          ],
-          "Especialidad": [
-            "Impresión",
-            "Diseño 3D"
-          ]
-        },
-        {
-          "Name": "Aiman GZ SL",
-          "Logo": "https://v5.airtableusercontent.com/v1/19/19/1690927200000/3An9B3dUNCW50m43cEuUVw/4jkmOUJRXOcgJPGWVr2tBVZdHrAQIejYDPbDGMocnFchXu_kfYYGuxKqnusd6-H7-CV5z5s1qu2iL5e5C2E_xhPz5clxI9ASjE8fCupHKv0/dwwfeBrD5s1IwCx2MkvYvoHJNwXev_fhJ3vIunWukos",
-          "Telefono": "91 886 05 01",
-          "Email": "info@aimangz.es",
-          "Region": "Madrid",
-          "Produccion": [
-            "Banderas"
-          ],
-          "Tecnologia": [
-            "Digital sobre rígidos"
-          ],
-          "Especialidad": [
-            "Diseño 3D"
-          ]
-        },
-        {
-          "Name": "Alta Rotulación ",
-          "Logo": "https://v5.airtableusercontent.com/v1/19/19/1690927200000/0H933yHbf8SlgUN-RugTeg/v5xjrGcIIQiuPI-YgFEboFVx380nllGjxwv2nF39et0GPr0bnnehE7z6vbZpqh2gmYsawYeWjxNcw6bjlJAPkGNxeZFpFJpSd8R85-eT4vruyZ-3JUaF3XhGGzM2IZu9/ZxtUBRlJPLucXU_Q88YspHeJWOufPmy0j2DETJupbUo",
-          "Telefono": "963117078",
-          "Email": "info@altarotulacion.com",
-          "Region": "Comunidad Valenciana",
-          "Provincia": "Valencia",
-          "Produccion": [
-            "Camisetas"
-          ],
-          "Tecnologia": [
-            "Tampografía"
-          ],
-          "Especialidad": [
-            "Impresión"
-          ]
-        },
-        {
-          "Name": "Alto impacto visual y PLV",
-          "Logo": "https://v5.airtableusercontent.com/v1/19/19/1690927200000/8lYRyLglAnyhv5PmiYfWfQ/FeCUILOG1G0F65HS8WtBtk67ffT92yePTr70buYJOwnP-f85dZnkq9rUYZihhsZ7NlN-J_hVH39W5_xnOKGjP-xNnlhZkwUb-1UoK9yu4r9Za8Ms7aqomQ_b0vrYhr8U/QNopK1l0v2B2LRwLO9ZZMGfZy-LJL0wvGinPAd7FyO4",
-          "Telefono": "910282810",
-          "Email": "info@altoimpactoplv.com",
-          "Region": "Madrid",
-          "Produccion": [
-            "PLV otros materiales"
-          ],
-          "Tecnologia": [
-            "Serigrafía"
-          ],
-          "Especialidad": [
-            "Diseño gráfico"
-          ]
-        }
+      {
+        "Name": "Adivin Banderas",
+        "Logo": "https://v5.airtableusercontent.com/v1/20/20/1692820800000/SVrxT6l11WyMu2hmgofYAg/Hqwf5tR8i8e35ryLJsBKG86eVUXiD0eiH_cReLDoabzAc0LXlhj4PtSOVxNMCx2eCNl6FrLsKSQuqPaipthRgg/HcWkRT2sD4F3QuPLQhcK5BcbdwxaH3hQuWR9z2E8EPU",
+        "Telefono": "952 31 60 22",
+        "Email": "jam@adivin.com",
+        "Region": "Andalucía",
+        "Provincia": "Córdoba",
+        "Produccion": [
+          "Publicidad exterior vallas y autobuses"
+        ],
+        "Tecnologia": [
+          "Serigrafía"
+        ],
+        "Especialidad": [
+          "Impresión"
+        ]
+      },
+      {
+        "Name": "3D Lenticular Factory",
+        "Logo": "https://v5.airtableusercontent.com/v1/20/20/1692820800000/YwXTuwiyGNfbSSxM7Ilegg/zLMZnv5Z8o0Eo4pXHxPFcsVIr1-TLsw5neoFNDkMBZJ_gdPDjkEfgMP4aPuEN_kZaUOOJhGsdd4lGG7S7CHpwA/rwh1fC6tZtwhuSlliNhDWMCn1q7QJRVrfssysSGKMso",
+        "Telefono": "917932125",
+        "Email": "info@3dlenticularfactory.com",
+        "Region": "Madrid",
+        "Produccion": [
+          "Impresión sobre soportes rígidos"
+        ],
+        "Tecnologia": [
+          "Diseño"
+        ],
+        "Especialidad": [
+          "Impresión",
+          "Diseño 3D"
+        ]
+      },
+      {
+        "Name": "Aiman GZ SL",
+        "Logo": "https://v5.airtableusercontent.com/v1/20/20/1692820800000/lHnKIn2DLHgP-odsd62ZDA/yM0VrFRWjMeGIqbTAJXvrfokCa1US88SQeruesKZ8vDlkzVB8Dm3hrWraZphpiCRafcASdfkqR6iz-lfKjaf6A/yYQQ3Jpx3o4eiZLs3ZiboIHyVbbQLEUfS_35nU6OLXg",
+        "Telefono": "91 886 05 01",
+        "Email": "info@aimangz.es",
+        "Region": "Madrid",
+        "Produccion": [
+          "Banderas"
+        ],
+        "Tecnologia": [
+          "Digital sobre rígidos"
+        ],
+        "Especialidad": [
+          "Diseño 3D"
+        ]
+      },
+      {
+        "Name": "Alta Rotulación ",
+        "Logo": "https://v5.airtableusercontent.com/v1/20/20/1692820800000/gwiSxZyY2_cWddlpOMZ3bQ/uutUE0qkNWlQaHDNXTNSQiFsSE2KqyLJKHM1HEGfJIC5dK9MESIvJ0G3PMFEeJ_erOXiSS11eXiCJevefzTaMQ/p-qYeSezspcvsfHL-o4-dnz2ZuetQedmO_LoLLjW0yk",
+        "Telefono": "963117078",
+        "Email": "info@altarotulacion.com",
+        "Region": "Comunidad Valenciana",
+        "Provincia": "Valencia",
+        "Produccion": [
+          "Camisetas"
+        ],
+        "Tecnologia": [
+          "Tampografía"
+        ],
+        "Especialidad": [
+          "Impresión"
+        ]
+      },
+      {
+        "Name": "Alto impacto visual y PLV",
+        "Logo": "https://v5.airtableusercontent.com/v1/20/20/1692820800000/0mdNHOMg7u4k8cSIl47YhA/JVC9hw5oTDmpPpzJfbVsya3_51y8gMFi9ajrZsDVOS5LTGFXYwtGE-vVRxpBU4txm7L4_U6HMyioLLE5aFuX0w/NpzxyR-g1TJOO4RFhj95SFeobh4g4Wi7Pex0V87kg2U",
+        "Telefono": "910282810",
+        "Email": "info@altoimpactoplv.com",
+        "Region": "Madrid",
+        "Produccion": [
+          "PLV otros materiales"
+        ],
+        "Tecnologia": [
+          "Serigrafía"
+        ],
+        "Especialidad": [
+          "Diseño gráfico"
+        ]
+      },
+      {
+        "Name": "Adivin Banderas",
+        "Logo": "https://v5.airtableusercontent.com/v1/20/20/1692820800000/SVrxT6l11WyMu2hmgofYAg/Hqwf5tR8i8e35ryLJsBKG86eVUXiD0eiH_cReLDoabzAc0LXlhj4PtSOVxNMCx2eCNl6FrLsKSQuqPaipthRgg/HcWkRT2sD4F3QuPLQhcK5BcbdwxaH3hQuWR9z2E8EPU",
+        "Telefono": "952 31 60 22",
+        "Email": "jam@adivin.com",
+        "Region": "Andalucía",
+        "Provincia": "Córdoba",
+        "Produccion": [
+          "Publicidad exterior vallas y autobuses"
+        ],
+        "Tecnologia": [
+          "Serigrafía"
+        ],
+        "Especialidad": [
+          "Impresión"
+        ]
+      },
+      {
+        "Name": "3D Lenticular Factory",
+        "Logo": "https://v5.airtableusercontent.com/v1/20/20/1692820800000/YwXTuwiyGNfbSSxM7Ilegg/zLMZnv5Z8o0Eo4pXHxPFcsVIr1-TLsw5neoFNDkMBZJ_gdPDjkEfgMP4aPuEN_kZaUOOJhGsdd4lGG7S7CHpwA/rwh1fC6tZtwhuSlliNhDWMCn1q7QJRVrfssysSGKMso",
+        "Telefono": "917932125",
+        "Email": "info@3dlenticularfactory.com",
+        "Region": "Madrid",
+        "Produccion": [
+          "Impresión sobre soportes rígidos"
+        ],
+        "Tecnologia": [
+          "Diseño"
+        ],
+        "Especialidad": [
+          "Impresión",
+          "Diseño 3D"
+        ]
+      },
+      {
+        "Name": "Aiman GZ SL",
+        "Logo": "https://v5.airtableusercontent.com/v1/20/20/1692820800000/lHnKIn2DLHgP-odsd62ZDA/yM0VrFRWjMeGIqbTAJXvrfokCa1US88SQeruesKZ8vDlkzVB8Dm3hrWraZphpiCRafcASdfkqR6iz-lfKjaf6A/yYQQ3Jpx3o4eiZLs3ZiboIHyVbbQLEUfS_35nU6OLXg",
+        "Telefono": "91 886 05 01",
+        "Email": "info@aimangz.es",
+        "Region": "Madrid",
+        "Produccion": [
+          "Banderas"
+        ],
+        "Tecnologia": [
+          "Digital sobre rígidos"
+        ],
+        "Especialidad": [
+          "Diseño 3D"
+        ]
+      },
+      {
+        "Name": "Alta Rotulación ",
+        "Logo": "https://v5.airtableusercontent.com/v1/20/20/1692820800000/gwiSxZyY2_cWddlpOMZ3bQ/uutUE0qkNWlQaHDNXTNSQiFsSE2KqyLJKHM1HEGfJIC5dK9MESIvJ0G3PMFEeJ_erOXiSS11eXiCJevefzTaMQ/p-qYeSezspcvsfHL-o4-dnz2ZuetQedmO_LoLLjW0yk",
+        "Telefono": "963117078",
+        "Email": "info@altarotulacion.com",
+        "Region": "Comunidad Valenciana",
+        "Provincia": "Valencia",
+        "Produccion": [
+          "Camisetas"
+        ],
+        "Tecnologia": [
+          "Tampografía"
+        ],
+        "Especialidad": [
+          "Impresión"
+        ]
+      },
+      {
+        "Name": "Alto impacto visual y PLV",
+        "Logo": "https://v5.airtableusercontent.com/v1/20/20/1692820800000/0mdNHOMg7u4k8cSIl47YhA/JVC9hw5oTDmpPpzJfbVsya3_51y8gMFi9ajrZsDVOS5LTGFXYwtGE-vVRxpBU4txm7L4_U6HMyioLLE5aFuX0w/NpzxyR-g1TJOO4RFhj95SFeobh4g4Wi7Pex0V87kg2U",
+        "Telefono": "910282810",
+        "Email": "info@altoimpactoplv.com",
+        "Region": "Madrid",
+        "Produccion": [
+          "PLV otros materiales"
+        ],
+        "Tecnologia": [
+          "Serigrafía"
+        ],
+        "Especialidad": [
+          "Diseño gráfico"
+        ]
+      },
+      {
+        "Name": "Adivin Banderas",
+        "Logo": "https://v5.airtableusercontent.com/v1/20/20/1692820800000/SVrxT6l11WyMu2hmgofYAg/Hqwf5tR8i8e35ryLJsBKG86eVUXiD0eiH_cReLDoabzAc0LXlhj4PtSOVxNMCx2eCNl6FrLsKSQuqPaipthRgg/HcWkRT2sD4F3QuPLQhcK5BcbdwxaH3hQuWR9z2E8EPU",
+        "Telefono": "952 31 60 22",
+        "Email": "jam@adivin.com",
+        "Region": "Andalucía",
+        "Provincia": "Córdoba",
+        "Produccion": [
+          "Publicidad exterior vallas y autobuses"
+        ],
+        "Tecnologia": [
+          "Serigrafía"
+        ],
+        "Especialidad": [
+          "Impresión"
+        ]
+      },
+      {
+        "Name": "3D Lenticular Factory",
+        "Logo": "https://v5.airtableusercontent.com/v1/20/20/1692820800000/YwXTuwiyGNfbSSxM7Ilegg/zLMZnv5Z8o0Eo4pXHxPFcsVIr1-TLsw5neoFNDkMBZJ_gdPDjkEfgMP4aPuEN_kZaUOOJhGsdd4lGG7S7CHpwA/rwh1fC6tZtwhuSlliNhDWMCn1q7QJRVrfssysSGKMso",
+        "Telefono": "917932125",
+        "Email": "info@3dlenticularfactory.com",
+        "Region": "Madrid",
+        "Produccion": [
+          "Impresión sobre soportes rígidos"
+        ],
+        "Tecnologia": [
+          "Diseño"
+        ],
+        "Especialidad": [
+          "Impresión",
+          "Diseño 3D"
+        ]
+      },
+      {
+        "Name": "Aiman GZ SL",
+        "Logo": "https://v5.airtableusercontent.com/v1/20/20/1692820800000/lHnKIn2DLHgP-odsd62ZDA/yM0VrFRWjMeGIqbTAJXvrfokCa1US88SQeruesKZ8vDlkzVB8Dm3hrWraZphpiCRafcASdfkqR6iz-lfKjaf6A/yYQQ3Jpx3o4eiZLs3ZiboIHyVbbQLEUfS_35nU6OLXg",
+        "Telefono": "91 886 05 01",
+        "Email": "info@aimangz.es",
+        "Region": "Madrid",
+        "Produccion": [
+          "Banderas"
+        ],
+        "Tecnologia": [
+          "Digital sobre rígidos"
+        ],
+        "Especialidad": [
+          "Diseño 3D"
+        ]
+      },
+      {
+        "Name": "Alta Rotulación ",
+        "Logo": "https://v5.airtableusercontent.com/v1/20/20/1692820800000/gwiSxZyY2_cWddlpOMZ3bQ/uutUE0qkNWlQaHDNXTNSQiFsSE2KqyLJKHM1HEGfJIC5dK9MESIvJ0G3PMFEeJ_erOXiSS11eXiCJevefzTaMQ/p-qYeSezspcvsfHL-o4-dnz2ZuetQedmO_LoLLjW0yk",
+        "Telefono": "963117078",
+        "Email": "info@altarotulacion.com",
+        "Region": "Comunidad Valenciana",
+        "Provincia": "Valencia",
+        "Produccion": [
+          "Camisetas"
+        ],
+        "Tecnologia": [
+          "Tampografía"
+        ],
+        "Especialidad": [
+          "Impresión"
+        ]
+      },
+      {
+        "Name": "Alto impacto visual y PLV",
+        "Logo": "https://v5.airtableusercontent.com/v1/20/20/1692820800000/0mdNHOMg7u4k8cSIl47YhA/JVC9hw5oTDmpPpzJfbVsya3_51y8gMFi9ajrZsDVOS5LTGFXYwtGE-vVRxpBU4txm7L4_U6HMyioLLE5aFuX0w/NpzxyR-g1TJOO4RFhj95SFeobh4g4Wi7Pex0V87kg2U",
+        "Telefono": "910282810",
+        "Email": "info@altoimpactoplv.com",
+        "Region": "Madrid",
+        "Produccion": [
+          "PLV otros materiales"
+        ],
+        "Tecnologia": [
+          "Serigrafía"
+        ],
+        "Especialidad": [
+          "Diseño gráfico"
+        ]
+      }
       ],
 
 };
